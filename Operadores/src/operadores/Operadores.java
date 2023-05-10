9fde0433979d0a4d8e81c250890eb6d03eb19778
KelvinQuezada <93734023+KelvinQuezada@users.noreply.github.com>
2023-05-10 12:57:26 -0500

    Add files via upload

---
+++ Operadores/src/operadores/Operadores.java
@@ -0,0 +1,36 @@
+/*
+OPERADORES Arimeticos EJERCICIO 2 POO
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadores;
+
+/**
+ *
+ * @author ESPE
+ */
+public class Operadores {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Metodo
+        int a=4;
+        int b=7;
+        
+        //int suma a+b 11
+        
+        int suma=a+b;
+        int resta=a-b;
+        int multiplicacion=a*b;
+        int division=a/b;
+        
+        // imprime los resultados de los valores Arimeticos
+        System.out.println(++suma);
+        System.out.println(resta);
+        System.out.println(multiplicacion);
+        System.out.println(division);
+        
+        
+    }
+    
+}
---
+++ PracticaUnoKpqb/src/practicaunokpqb/PracticaUnoKpqb.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaunokpqb;
+
+/**
+ *
+ * @author ESPE
+ */
+public class PracticaUnoKpqb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("El numero es: ");
+        //metodos 
+         byte a=4;
+         short b=200;
+         int c=100000;
+         long d=1000000000;
+         // caracteres
+         char t='T';
+         double f=4.5;
+         float g=4.5f;
+         String h= "hola mundo";
+         boolean i =5 >10;
+         System.out.println(g);
+         System.out.println(a);
+         System.out.println(i);
+         
+         
+         
+         
+         
+        
+    }
+    
+}
